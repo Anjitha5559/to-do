1eaa388b18e61b54837bb81e2dfe6b03a2db6781
Anjitha5559 <84130915+Anjitha5559@users.noreply.github.com>
2022-05-28 15:18:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,76 @@ function id(id) {
 function clss(cls) {
     return document.getElementsByClassName(cls);
 }
+get();
 
-function log(val) {
-    console.log(val);
+addclosefunction();
+
+function addclosefunction() {
+    var close = document.getElementsByClassName("closebtn");
+    for (var i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+            var listt = JSON.parse(localStorage.getItem('todoapp'));
+            for (var i = 0; i < listt.length; i++) {
+                if (listt[i].stamp == this.id) {
+
+                    listt.splice(i, 1);
+                    localStorage.setItem('todoapp', JSON.stringify(listt));
+                }
+            }
+
+
+        }
+    }
 }
-
 
 function addtodo() {
     var cat = id("formcategory").value;
     var txt = id("Enterfield").value;
     var time = id("Timefield").value;
-    console.log(cat + ":" + txt + ":" + time);
-    id("entries").innerHTML += '  <div class="slots"> <div class="cat">' + cat + '</div><div class="txt">' + txt + '</div><div class="time">' + time + '</div><div class="closebtn">X</div></div>'
+    if (txt == '') {
+        alert("Enter the subject");
+        return;
+    }
+
+    if (time == '') {
+        alert("Enter time");
+        return;
+    }
+    const d = new Date();
+    let timenw = d.getTime();
+    id("entries").innerHTML += '  <div class="slots" id="' + txt + timenw + '"> <div class=" cat ">' + cat + '</div><div class=" txt ">' + txt + '</div><div class=" time ">' + time + '</div><div class=" closebtn">X</div></div>'
+    save(cat, txt, time, timenw);
+    id("formcategory").value = '';
+    id("Enterfield").value = '';
+    id("Timefield").value = '';
+
+}
+
+function save(cat, txt, time, timenw) {
+    var listt = JSON.parse(localStorage.getItem('todoapp'));
+    if (listt == null) {
+        listt = [];
+    }
+    var data = {
+        category: cat,
+        text: txt,
+        tme: time,
+        stamp: timenw
+    };
+    listt.push(data);
+    localStorage.setItem("todoapp", JSON.stringify(listt));
+
+
+}
+
+function get() {
+    var listt = JSON.parse(localStorage.getItem('todoapp'));
+    if (listt == null) {
+        listt = [];
+    }
+    for (var i = 0; i < listt.length; i++) {
+        id("entries").innerHTML += '  <div class="slots"> <div class="cat">' + listt[i].category + '</div><div class="txt">' + listt[i].text + '</div><div class="time">' + listt[i].tme + '</div><div class="closebtn" id="' + listt[i].stamp + '">X</div></div>'
+    }
 }
